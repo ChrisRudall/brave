204d437c8c2ea719b270bf6567bf07c9bfe321ba
Chris <chris@indemandmusic.com>
2019-12-28 19:27:14 +0000

    Update Client.cpp

--- gst-WebRenderSrc/src/cef/Client.cpp
+++ gst-WebRenderSrc/src/cef/Client.cpp
@@ -13,7 +13,8 @@ Client::~Client()
 bool Client::GetViewRect(CefRefPtr<CefBrowser> browser, CefRect& rect)
 {
     //Call listener
-    return listener && listener->GetViewRect(rect);
+    #return listener && 
+        listener->GetViewRect(rect);
 }
 
 void Client::OnPaint(CefRefPtr<CefBrowser>, CefRenderHandler::PaintElementType type, const RectList& rects, const void* buffer, int width, int height)
